9ab66756f1fe320c16c522883c56f2151b722d53
Colin <colin.shortland@gmail.com>
2019-02-26 19:40:06 +1000

    initial commit

---
+++ main.c
@@ -0,0 +1,25 @@
+/*
+ * File   : main.c
+ * Author : Colin Shortland
+ * Date.  : February 26, 2019 (19:11)
+ *
+ * Description:
+ *  The classic program.
+ *
+ */
+#include <stdio.h>
+
+int main() {
+    printf("\nHello, World!");
+    printf("\nHello, World!");
+    printf("\nHello, World!");
+    printf("\nHello, World!");
+    printf("\nHello, World!\n");
+
+    return 0;
+}
+
+/*
+ * end of file: main.css
+ *
+ */
